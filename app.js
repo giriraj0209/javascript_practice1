9046d21ad5a2b7801a82a6715fb8aff502e9ac94
giriraj0209 <giriraj.kashyapa@gmail.com>
2017-07-09 22:34:05 -0500

    JS changes

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 function countryInfo(country){
 var c = country.getAttribute('id');
-alert(c);
+
+if(c =="ind"){
+country.width =150;
+country.height=150;
+document.getElementById("text1").setAttribute("style", "display:block");
+}
+}
+
+function countryImg(country){
+country.width =140;
+country.height=140;
+document.getElementById("text1").setAttribute("style", "display:none");
 }
